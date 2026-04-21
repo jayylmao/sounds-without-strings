ac9dc4f4ba31a9ae28a92b1e0279df2a0ac2316c
jayylmao <jaymichael_carlos@icloud.com>
2026-04-21 22:06:02 +0800

    feat: add live camera feed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Sounds Without Strings
 
+import time
+
 import cv2
 import mediapipe as mp
 import mido
@@ -14,13 +16,14 @@ from mediapipe.tasks.python.vision.gesture_recognizer import (
     GestureRecognizerOptions,
     GestureRecognizerResult,
 )
+from numpy.random.mtrand import f
 
-MODEL_PATH = "/model/gesture_recognizer.task"
+MODEL_PATH = "model/gesture_recognizer.task"
 
 mido.set_backend("mido.backends.rtmidi")
 
 try:
-    midi_out = mido.open_output("Python MIDI 1")  # type: ignore
+    midi_out = mido.open_output("IAC Driver Python MIDI 1")  # type: ignore
 except OSError:
     print("Available ports:", mido.Backend().get_output_names())
     print("Please change the port name in the code to match one of the above.")
@@ -40,4 +43,26 @@ options = GestureRecognizerOptions(
 )
 
 with GestureRecognizer.create_from_options(options) as recognizer:
-    pass
+    cap = cv2.VideoCapture(1)
+
+    while cap.isOpened():
+        success, frame = cap.read()
+        if not success:
+            print("[*] Empty camera frame")
+            continue
+
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+
+        frame_timestamp_ms = int(time.time() * 1000)
+
+        recognizer.recognize_async(mp_image, frame_timestamp_ms)
+
+        cv2.imshow("Sounds Without Strings", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
